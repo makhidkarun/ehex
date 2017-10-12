611d69292d18ebe9aa213010bb00aba1fc8074dc
Arthur Green <arthur@phraction.org>
2017-10-12 16:30:48 +0100

    Fix URL in setup.py, move to v0.1.1

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup
 
 setup(name='ehex',
-      version='0.1',
+      version='0.1.1',
       description='eHex module (from Traveller/Cepheus System)',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/makhidkarun/ehex',
       author='Arthur Green',
       author_email='arthur.green045@gmail.com',
       license='MIT',
